00de92504eeb1f8674b91eb4e52109aee9f3a743
mobjengine <mobjengine@users.noreply.github.com>
2025-08-25 16:07:20 -0500

    Adding src files

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC	=	gcc
+CFLAGS	=	-std=gnu99 -Wall $(shell pkg-config --cflags sdl2)
+LIBS	=	$(shell pkg-config --libs sdl2)
+EXE	=	koronome
+OBJS	=	\
+	k_main.o
+
+all:	$(EXE)
+
+clean:
+	rm -f $(OBJS)
+
+$(EXE):	$(OBJS)
+	$(CC) $(CFLAGS) -o $(EXE) $(OBJS) $(LIBS)
+
+%.o:	%.c
+	$(CC) $(CFLAGS) -c -o $@ $<
---
+++ k_main.c
@@ -0,0 +1,46 @@
+/*
+koronome - Raycasting dungeon crawler with grid-based movement.
+Copyright (C) 2025  mobjengine
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <SDL.h>
+#include "k_main.h"
+
+int main(int argc, const char **argv) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("KORONOME", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Event event;
+    SDL_bool quit = SDL_FALSE;
+    do {
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_QUIT:
+                    quit = SDL_TRUE;
+                    break;
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+    } while(!quit);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+};
